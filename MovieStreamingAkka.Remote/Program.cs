850ff593fbc0f2b840f7c430b1eb47552acafd2b
renan <renan@interprocess.com.br>
2022-06-15 10:14:46 -0300

    feat: add console application moviestreaming remote

---
+++ MovieStreamingAkka.Remote/Program.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreamingAkka.Remote
+{
+    internal class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Creating MovieStreamingActorSystem in remote process");
+
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            MovieStreamingActorSystem.WhenTerminated.Wait();
+        }
+    }
+}
